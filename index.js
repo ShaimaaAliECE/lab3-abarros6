f6076b5327e4a93e4490ed352ea7fcb81dacac8a
Anthony Barros <abarros6@uwo.ca>
2021-11-18 18:58:21 -0500

    data can now be added and displayed

--- index.js
+++ index.js
@@ -6,13 +6,66 @@ const newConnection = require('./DBConnection');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.sendFile('/frontend/index.html', { root: __dirname })
-  })
+app.get('/timeDisplay', (req, res) => {
+  let conn = newConnection();
+  conn.connect();
+  let timeList;
+  conn.query(`select * from Time`, (err,rows,fields) => {
+
+      if (err)
+          res.send('ERROR: ' +err)
+      else
+      {
+          timeList = rows;
+
+          let content ='';
+          for (t of timeList)
+          {
+              content += '<div>';
+              content += t.T1 + " : " + t.T2 + " : " + t.T3 + " : " + t.T4 + " : " + t.T5 + " : " + t.T6 + " : " + t.T7 + " : " + t.T8 + " : " + t.T9 + " : " + t.T10 
+              content += '</div>'
+              content += '\n';
+          }
+
+          res.send(content);
+      }
+  })    
+})
+
+app.get('/userDisplay', (req, res) => {
+  let conn = newConnection();
+  conn.connect();
+  let userList;
+  conn.query(`select * from Users`, (err,rows,fields) => {
+
+      if (err)
+          res.send('ERROR: ' +err)
+      else
+      {
+          userList = rows;
+
+          let content ='';
+          for (u of userList)
+          {
+              content += '<div>';
+              content += u.Name + " : " + u.T1 + " : " + u.T2 + " : " + u.T3 + " : " + u.T4 + " : " + u.T5 + " : " + u.T6 + " : " + u.T7 + " : " + u.T8 + " : " + u.T9 + " : " + u.T10 
+              content += '</div>'
+              content += '\n';
+              content += '\n';
+          }
+
+          res.send(content);
+      }
+  })    
+})
 
 app.use(express.urlencoded({
   extended: true
 }))
+
+app.get('/', (req, res) => {
+  res.sendFile('/frontend/index.html', { root: __dirname })
+})
 
 app.get('/guest', (req, res) => {
   res.sendFile('/frontend/guest.html', { root: __dirname })
@@ -22,39 +75,27 @@ app.get('/admin', (req, res) => {
   res.sendFile('/frontend/admin.html', { root: __dirname })
 })
 
-app.post('/login', (req, res) => {
-  let userName = req.body.usr;
-  let password = req.body.pwd;
-  let message = "Access denied";
-
-  if (userName == 'admin' && password == '123') {
-    message = "Welcome";
-    res.sendFile('/frontend/admin.html', { root: __dirname })
-  }
-
-  res.send(message)
-})
-
 app.get('/login', (req, res) => {
-  let userName = req.body.usr;
-  let password = req.body.pwd;
-  let message = "Access denied";
+  let userName = req.query.usr;
+  let password = req.query.pwd;
 
   if (userName == 'admin' && password == '123') {
     message = "Welcome";
     res.sendFile('/frontend/admin.html', { root: __dirname })
   }
+  else 
+  {
+    res.sendFile('/frontend/index.html', { root: __dirname })
+  }
 
-  res.send(message)
 })
 
 app.get('/add-user', (req,res) => {
   let conn = newConnection();
   conn.connect();
-  console.log(req.query.T1);
   conn.query(`insert into Users values ('${req.query.name}','${req.query.T1}','${req.query.T2}','${req.query.T3}','${req.query.T4}','${req.query.T5}','${req.query.T6}','${req.query.T7}','${req.query.T8}','${req.query.T9}','${req.query.T10}')`
           ,(err,rows,fields) => {
-              res.redirect('/');        
+              res.redirect('/userDisplay');        
           } );
 
   conn.end();
@@ -63,21 +104,19 @@ app.get('/add-user', (req,res) => {
 app.get('/add-times', (req,res) => {
   let conn = newConnection();
   conn.connect();
-  console.log(req.query.T1);
   conn.query( `UPDATE Time SET  T1 = '${req.query.T1}', T2 = '${req.query.T2}', T3 = '${req.query.T3}', T4 = '${req.query.T4}', T5 = '${req.query.T5}', T6 = '${req.query.T6}', T7 = '${req.query.T7}', T8 = '${req.query.T8}', T9 = '${req.query.T9}', T10 = '${req.query.T10}'`
           , (err,rows,fields) => {
               if (err)
                   console.log(err);
               else
                   console.log('row updated');
-                  res.redirect('/');
+                  res.redirect('/timeDisplay');
           });
 
   conn.end();
 })
 
 app.use(express.static('frontend'))
-
 // make the app listen to port 80 as specified in the project instructions
 // part 6 of the development section adresses this 
 app.listen(80);
