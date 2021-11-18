e5ebe58533a8fdab9647ea8bb13248bc227f857b
Anthony Barros <abarros6@uwo.ca>
2021-11-18 17:12:06 -0500

    working on db integration

--- index.js
+++ index.js
@@ -18,6 +18,10 @@ app.get('/guest', (req, res) => {
   res.sendFile('/frontend/guest.html', { root: __dirname })
 })
 
+app.get('/admin', (req, res) => {
+  res.sendFile('/frontend/admin.html', { root: __dirname })
+})
+
 app.post('/login', (req, res) => {
   let userName = req.body.usr;
   let password = req.body.pwd;
@@ -54,6 +58,19 @@ app.get('/add-user', (req,res) => {
           } );
 
   conn.end();
+})
+
+app.get('/add-times', (req,res) => {
+  let conn = newConnection();
+  conn.connect();
+  console.log("hmmmmmmm");
+  conn.query(`UPDATE Time SET T1 = ${req.query.name}, T2 = ${req.query.name}, T3 = ${req.query.name}, T4 = ${req.query.name}, T5 = ${req.query.name}, T6 = ${req.query.name}, T7 = ${req.query.name}, T8 = ${req.query.name}, T9 = ${req.query.name}, T10 = ${req.query.name},)`
+          ,(err,rows,fields) => {
+              res.redirect('/');        
+          } );
+
+  conn.end();
+  console.log('connection ended');
 })
 
 app.use(express.static('frontend'))
--- initDB.js
+++ initDB.js
@@ -76,7 +76,7 @@ conn.query(`CREATE TABLE Users
                     console.log('Table Created');
             })
 
-conn.query( `insert into Time values (0,1,2,3,4,5,6,7,8,9)`
+conn.query( `insert into Time values (0,0,0,0,0,0,0,0,0,0)`
             , (err,rows,fields) => {
                 if (err)
                     console.log(err);
